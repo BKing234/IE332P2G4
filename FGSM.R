d17288225b649e5047ca5bc8ca6805e5aff077c9
jackperrotta1 <131020023+jackperrotta1@users.noreply.github.com>
2023-04-27 19:28:52 -0400

    Update FGSM.R

--- FGSM.R
+++ FGSM.R
@@ -1,10 +1,3 @@
-library(tidyverse)
-library(keras)
-library(tensorflow)
-library(reticulate)
-install_tensorflow(extra_packages="pillow")
-install_keras()
-
 # Load the original model
 model <- load_model_tf("/home/jupyter/332_data/dandelion_model")
 
@@ -21,11 +14,11 @@ epsilon <- 0.01
 fgsm_attack <- function(image, label, epsilon, model) {
   # Convert the image to an array
   x <- image_to_array(image)
-  # Reshape the array to match the input shape of the model
+  # Reshape the array
   x <- array_reshape(x, c(1, dim(x)))
-  # Scale the pixel values to the range [0, 1]
+  # Scale th pixels to fit in the range [0, 1]
   x <- x / 255
-  # Compute the loss and gradients for the input images and labels
+  # Compute the loss and gradients
   label_dandelion <- c(1, 1)
   label_grass <- c(1, 2)
   loss_dandelion <- function(x) {
@@ -39,7 +32,7 @@ fgsm_attack <- function(image, label, epsilon, model) {
   # Compute the sign of the gradients
   sign_grad_dandelion <- sign(grad_dandelion)
   sign_grad_grass <- sign(grad_grass)
-  # Compute the perturbation as the epsilon times the sign of the gradients
+  # Compute the perturbation
   if (model(x)[[1]]$class_name == "dandelion") {
     perturbation <- epsilon * sign_grad_grass
   } 
@@ -58,15 +51,15 @@ fgsm_attack <- function(image, label, epsilon, model) {
 }
 
 # Define function to modify only 1% of the pixels in the image
-mod_image <- function(image, percent) {
+modified_image <- function(image, percent) {
   # Calculate the number of pixels in the image
-  n_pixels <- prod(dim(I_adv)[-1])
+  num_pixels <- prod(dim(I_adv)[-1])
   # Calculate the number of pixels to modify
-  n_mod_pixels <- round(n_pixels * percent / 100)
+  num_modified_pixels <- round(num_pixels * percent / 100)
   # Choose a random subset of pixels to modify
-  mod_pixels <- sample(n_pixels, n_mod_pixels, replace = FALSE)
+  modified_pixels <- sample(num_pixels, num_modified_pixels, replace = FALSE)
   # Modify the selected pixels by adding a small random value
-  I_adv[[1]][mod_pixels] <- I_adv[[1]][mod_pixels] + runif(n_mod_pixels, -10, 10)
+  I_adv[[1]][modified_pixels] <- I_adv[[1]][modified_pixels] + runif(num_modified_pixels, -10, 10)
 
   # Return the modified adversarial image
   return(I_adv)
