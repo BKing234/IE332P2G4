f1095a973518204ba8dc2d1758e5287634011478
jackperrotta1 <131020023+jackperrotta1@users.noreply.github.com>
2023-04-27 12:51:46 -0400

    Update FGSM.R

--- FGSM.R
+++ FGSM.R
@@ -60,23 +60,14 @@ fgsm_attack <- function(image, label, epsilon, model) {
 # Define function to modify only 1% of the pixels in the image
 mod_image <- function(image, percent) {
   # Calculate the number of pixels in the image
-  n_pixels <- prod(dim(image)[-1])
+  n_pixels <- prod(dim(I_adv)[-1])
   # Calculate the number of pixels to modify
   n_mod_pixels <- round(n_pixels * percent / 100)
   # Choose a random subset of pixels to modify
   mod_pixels <- sample(n_pixels, n_mod_pixels, replace = FALSE)
   # Modify the selected pixels by adding a small random value
-  mod_image <- image
-  mod_image[[1]][mod_pixels] <- mod_image[[1]][mod_pixels] + runif(n_mod_pixels, -10, 10)
-  return(mod_image)
+  I_adv[[1]][mod_pixels] <- I_adv[[1]][mod_pixels] + runif(n_mod_pixels, -10, 10)
+
+  # Return the modified adversarial image
+  return(I_adv)
 }
-
-# Load an example image of grass
-grass_file <- list.files("/home/jupyter/332_data/data-for-332/grass/")[1]
-grass_image <- image_load(paste("/home/jupyter/332_data/data-for-332/grass/", grass_file, sep = ""),
-                           target_size = target_size)
-
-# Generate an adversarial example by modifying only 1% of the pixels
-dandelion_label <- c(1, 0)
-adv_grass_image <- modify_image(grass_image, 1)
-adv_grass_image <- fgsm_attack(adv_grass_image, dandelion_label, epsilon, model)
